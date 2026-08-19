fd70509fa14054cde2419e1fddea38f7ce35287b
AliceInHunterland <35739873+AliceInHunterland@users.noreply.github.com>
2026-08-19 16:48:53 +0900

    Update Homebrew cask for v0.1.5 (#14)
    Promote the checksummed cask generated from the published v0.1.5 release assets.
    
    Signed-off-by: Ekaterina Pavlova <ekaterina@openorigins.com>

--- Casks/tally.rb
+++ Casks/tally.rb
@@ -1,9 +1,9 @@
 cask "tally" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.1.4"
-  sha256 arm:   "9d28cd8133d33e55ab4303f17c32db9dfe50e704f023b4f1bcec061104d0c050",
-         intel: "488f376e266e3456abd50f88e6bc666ae75b0287dc579c09843e85c4fbfa35b7"
+  version "0.1.5"
+  sha256 arm:   "dc2912e9e79187d33234f82c4c41ba73dbeabb0a6834c54faab18a8ae34ba2e3",
+         intel: "3444338fcc22abd1e3ba4bddc876516e2ee3e3aa45defdd0110c5f99235c58c3"
 
   url "https://github.com/OpenOrigins/tally/releases/download/v#{version}/tally-macos-#{arch}.dmg"
   name "Tally"
