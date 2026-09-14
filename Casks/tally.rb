df9a75f98811a87bd523a0974f01ec4a0f591a92
Ekaterina Pavlova <ekaterina@openorigins.com>
2026-09-14 20:16:29 +0300

    Update Homebrew cask for v0.1.13
    Signed-off-by: Ekaterina Pavlova <ekaterina@openorigins.com>

--- Casks/tally.rb
+++ Casks/tally.rb
@@ -1,9 +1,9 @@
 cask "tally" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.1.12"
-  sha256 arm:   "620d3bb0f537805d2b04174e402348601fd3c5a630c192203244ca4ffee185f8",
-         intel: "1878a92138e22414c877e70e1a75cdb541674822388851390e642dd4f6f8fa5d"
+  version "0.1.13"
+  sha256 arm:   "08536d2166314b2b80934f5ef102a914875a4dbc7a4b932315ed9a85ab4067c7",
+         intel: "d9c0f325ca3df0345431de391038687993308910cef465fca5ef76c1786bf338"
 
   url "https://github.com/OpenOrigins/tally/releases/download/v#{version}/tally-macos-#{arch}.dmg"
   name "Tally"
